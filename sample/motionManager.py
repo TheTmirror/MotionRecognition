0dc15f3d2db61acaf784d02fac5dd9e8a5e501f5
Tristan Seidl <tristan.gamer@web.de>
2018-07-27 13:59:09 +0200

    Der MotionManager wurde ähnlich des DevicesManagers umgestaltet

--- sample/motionManager.py
+++ sample/motionManager.py
@@ -1,16 +1,24 @@
+# -*- coding: utf-8 -*-
+#System
 import os
+
+#Project
 from dataManager import DataManager
+from motion import Motion
 
 class MotionManager:
 
     TEMPLATES_PATH = '/home/pi/Desktop/Updated Project/templates/'
+    __motions = dict()
+    __motionFiles = dict()
 
     def __init__(self):
-        self.dm = DataManager()
+        pass
 
-    def loadMotions(self):
-        motions = []
-
+    #Ggf. automatisiert möglich?
+    def initMotions(self):
+        dm = DataManager()
+        #Hier alle Devices intitialisieren
         oldPath = os.getcwd()
         try:
             os.chdir(self.TEMPLATES_PATH)
@@ -24,12 +32,19 @@ class MotionManager:
             if os.path.isdir(filePath):
                 continue
 
-            motion = self.dm.getMotion(filePath)
-            motions.append(motion)
+            motion = dm.getMotion(filePath)
+            self.__motions[motion.getName()] = motion
+            self.__motionFiles[motion.getName()] = file
 
         os.chdir(oldPath)
 
-        return motions
+    def addMotions(self, newMotions):
+        for motion in newMotions:
+            self.addMotion(motion)
+
+    def addMotion(self, newMotion):
+        self.__motions[newMotion.getName()] = newMotion
+        self.saveMotion(newMotion)
 
     def saveMotion(self, template):
         dm = DataManager()
@@ -44,5 +59,24 @@ class MotionManager:
         if not os.path.exists(os.path.dirname(self.TEMPLATES_PATH)):
             os.makedirs(os.path.dirname(self.TEMPLATES_PATH))
 
-        self.dm.saveMotion(template, plainPath)
+        dm.saveMotion(template, plainPath)
         print('Motion Saved')
+
+    #Sollte von der Logik her wahrscheinlich in saveMotion mit rein
+    def updateMotion(self, motion):
+        dm = DataManager()
+        path = self.TEMPLATES_PATH + self.getMotionFile(motion.getName())
+        dm.saveMotion(motion, path)
+        print('Motion Updated')
+
+    def getAllMotions(self):
+        return self.__motions
+
+    def getMotion(self, key):
+        return self.__motions[key]
+
+    def getAllMotionFiles(self):
+        return self.__motionFiles
+
+    def getMotionFile(self, key):
+        return self.__motionFiles[key]
